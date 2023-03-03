66b2d508fbb0598b92145d965c42359042d0e91a
Ramon Barbosa <ramonbarbosafreitas@gmail.com>
2023-03-03 17:39:55 -0300

    feat: implementos na aula 1 do curso

--- aula_1_curiosidades.py
+++ aula_1_curiosidades.py
@@ -1,6 +1,18 @@
 # Aula 1: Curiosidades da linguagem Python
 
+# O nome Python é derivado da série de tv Monty Python`s flying circus disponivel na netflix,
+# e o fundador da linguagem, cuja série favorita era esta é o Guido Van Rossum.
+
+# Linguagem interpretada, ou seja, não necessita de um compilador e nem gerar um bytecode
+
+# Possui variantes para linguagem C e Java, CPython e Jython respectivamente
+
+# else em estrutura de repetição for
+
+# Funções podem retornar uma estrutura de dados com vários dados ao invés de apenas 1 dado
+
 import this
+
 
 numero = 123_456_789
 print(numero)
